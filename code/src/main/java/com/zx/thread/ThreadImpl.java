2d467e4a583058787da1ffbd33b6d3d2767697fc
zhangxin <zhangxin@agilestar.cn>
2026-03-29 08:46:29 +0800

    Add:多线程方法demo

--- code/src/main/java/com/zx/thread/ThreadImpl.java
+++ code/src/main/java/com/zx/thread/ThreadImpl.java
@@ -4,7 +4,16 @@ public class ThreadImpl extends Thread{
 
     @Override
     public void run() {
-        System.out.println("任务完成");
+        int num = 0;
+        while(num < 2000){
+            if(Thread.currentThread().isInterrupted()){
+                System.out.println("[检测到中断标识][任务中断]");
+                break;
+            }
+            Thread thread = Thread.currentThread();
+            System.out.println(thread.getName() + "[正在执行][任务号："+num+"]");
+            num++;
+        }
     }
 
     public static void main(String[] args) {
